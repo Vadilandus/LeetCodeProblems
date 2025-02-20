58df8f58f98b2c857efd796d2062ed83fb838b1d
Vadim <antonuk_proo@mail.ru>
2025-02-20 21:31:57 +0300

    Added PlusOne

---
+++ PlusOne/PlusOne/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        int Length = RealizeLength(digits);
+        int[] result = new int[Length];
+        digits = digits.Reverse().ToArray();
+        int index = 1;
+
+        for (int i = 0; i < digits.Length; i++)
+        {
+            result[i] = digits[i] + index;
+            index = 0;
+            if (result[i] == 10 && i + 1 != digits.Length)
+            {
+                result[i] = 0;
+                index = 1;
+            }
+            if (result[i] == 10 && i + 1 == digits.Length)
+            {
+                index = 1;
+                result[i] = 0;
+                result[i + 1] = index;
+            }
+
+
+
+        }
+
+        return result.Reverse().ToArray();
+    }
+    public int RealizeLength(int[] digits)
+    {
+        for (int s = 0; s < digits.Length; s++)
+        {
+            if (digits[s] != 9)
+            {
+                return digits.Length;
+            }
+        }
+        return digits.Length + 1;
+    }
+}
